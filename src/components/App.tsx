cdc7d1343164c4db2cc26830db5e6dbbdcc9d37e
Jacob <jacob.saini60@gmail.com>
2021-07-23 09:24:32 +1000

    init commit

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import  "./styles.scss"
+
+const App: React.FC = () => {
+  return (
+ 
+  <h1>React</h1>
+    );
+  };
+export default App
---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebPackPlugin from "html-webpack-plugin";
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+const config: webpack.Configuration = {
+  mode: "production",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname,  "/dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json",".scss"]
+  },
+
+  module: {
+    rules: [
+     
+      { test: /\.tsx?$/,  use: ["ts-loader"]  },
+      { test:/\.scss$/,  use: ["style-loader","css-loader", "sass-loader"]  }
+     
+    ]
+  },
+  plugins: [htmlPlugin]
+};
+
+export default config;
